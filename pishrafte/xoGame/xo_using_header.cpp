d5891ef6eeb4ee93aef3e996570a2bf63cf285a3
Mohsen Dastjerdi Zade <mail@dstjrd.ir>
2014-11-14 16:35:45 +0330

    Moved files to own directories. made a heade file 'xoGame.h'

---
+++ pishrafte/xoGame/xo_using_header.cpp
@@ -0,0 +1,19 @@
+/*
+ * simple 3x3 XO game.
+ * Object Oriented version.
+ * source available at (Github repo - MIT licence):
+ * http://dstjrd.ir/s/cppx
+ * https://github.com/mohsend/cpp-examples
+*/
+
+#include <iostream>
+#include "xoGame.h"
+
+using namespace std;
+
+int main()
+{
+	xoGame xo;
+	xo.play();
+	return 0;
+}
