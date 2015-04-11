c7e37501e6e3e5aaddeb63378d4fe980dc1cdcfb
Mohsen Dastjerdi Zade <mail@dstjrd.ir>
2015-04-11 22:12:34 +0430

    jalase 5

---
+++ graphics_1/j05-01-front.cpp
@@ -0,0 +1,41 @@
+/*
+ * 
+ */
+
+#include <GL/glut.h>
+using namespace std;
+
+void display()
+{
+	glClear(GL_COLOR_BUFFER_BIT);
+	//
+	gluLookAt(0.0,0.0,0.0,0.0,0.0,1.0,0.0,1.0,0.0);
+	//
+	//glutWireCube(1.0);
+	glutWireTeapot(1.0);
+	glFlush();
+}
+
+void init()
+{
+	glClearColor(0.0,0.0,0.0,0.0);
+	glColor3f(1.0,0.0,0.0);
+	glMatrixMode(GL_PROJECTION);
+	glLoadIdentity();
+	// 
+	glOrtho(-4.0,4.0,-4.0,4.0,-4.0,4.0);
+	glMatrixMode(GL_MODELVIEW);
+	glLoadIdentity();
+}
+
+int main(int argc, char **argv)
+{
+	glutInit(&argc, argv);
+	glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
+	glutInitWindowSize(500,500);
+	glutInitWindowPosition(0,0);
+	glutCreateWindow("tasvir e isometric");
+	glutDisplayFunc(display);
+	init();
+	glutMainLoop();
+}
---
+++ graphics_1/j05-02.cpp
@@ -0,0 +1,43 @@
+/*
+ * 
+ */
+
+#include <GL/glut.h>
+using namespace std;
+
+int singleb, doubleb;
+
+void display()
+{
+
+}
+
+void init()
+{
+	glClearColor(0.0,0.0,0.0,0.0);
+	glColor3f(1.0,0.0,0.0);
+	glMatrixMode(GL_PROJECTION);
+	glLoadIdentity();
+	// 
+	glOrtho(-4.0,4.0,-4.0,4.0,-4.0,4.0);
+	glMatrixMode(GL_MODELVIEW);
+	glLoadIdentity();
+}
+
+int main(int argc, char **argv)
+{
+	glutInit(&argc, argv);
+	glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
+	glutInitWindowSize(400,400);
+	glutInitWindowPosition(0,0);
+	singleb = glutCreateWindow("window single");
+	glutDisplayFunc(display1);
+  
+  glutCreateSubWindow(single, 100,100,300,300);
+  glutDisplayFunc(display1);
+  glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGBA);
+  
+  
+	init();
+	glutMainLoop();
+}
