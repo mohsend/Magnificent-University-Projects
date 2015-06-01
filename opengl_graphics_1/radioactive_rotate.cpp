5b73feeb7f3adadf2a4ca13f333e4f25db01aca5
Mohsen Dastjerdi Zade <mail@dstjrd.ir>
2015-06-01 11:01:29 +0430

    an awesome red flash

--- opengl_graphics_1/radioactive_rotate.cpp
+++ opengl_graphics_1/radioactive_rotate.cpp
@@ -1,4 +1,5 @@
 #include <GL/glut.h>
+#include <iostream>
 #include <cmath>
 
 #define PI 3.14159265359
@@ -8,7 +9,7 @@ using namespace std;
 int deg = 0;
 
 void radioactive();
-void rotate();
+void time_pass();
 void init();
 
 int main(int argc, char **argv)
@@ -18,7 +19,7 @@ int main(int argc, char **argv)
 	glutInitWindowSize(500,500);
 	glutInitWindowPosition(0,0);
 	glutCreateWindow("Radioactive");
-	glutIdleFunc(rotate);
+	glutIdleFunc(time_pass);
 	glutDisplayFunc(radioactive);
 	init();
 	glutMainLoop();
@@ -34,13 +35,16 @@ void init()
 void radioactive()
 {
 	glClear(GL_COLOR_BUFFER_BIT);
-	glColor3f(0.0, 0.0, 0.0);
 	glRotatef(deg, 0.0, 0.0, 1.0);
+	
+	float red = cos (2.0 * PI * (deg/120.0));
 	
 	for (double d = 0.0; d < 2.0*PI; d += 2*PI/3.0)
 	{
 		glBegin(GL_TRIANGLE_FAN);
+			glColor3f(red, 0.0, 0.0);
 			glVertex2d(0.0,0.0);
+			glColor3f(0.0, 0.0, 0.0);
 			for (double dis = 0.0; dis < 2*PI/6; dis += 0.1)
 			{
 				glVertex2d(cos(d + dis), sin(d + dis));
@@ -52,7 +56,7 @@ void radioactive()
 	glutSwapBuffers();
 }
 
-void rotate()
+void time_pass()
 {
 	deg = (deg < 120) ? deg + 1 : 0;
 	glutPostRedisplay();
