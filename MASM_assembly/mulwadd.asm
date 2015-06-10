5c46da6083a2c33120f6ad1f3969a8a16b461f2d
Mohsen Dastjerdi Zade <mail@dstjrd.ir>
2015-06-10 23:21:45 +0430

    code to multiply using LOOP and ADD

--- MASM_assembly/mulwadd.asm
+++ MASM_assembly/mulwadd.asm
@@ -1,4 +1,5 @@
-; this is the template of a 8086 MASM assembly code
+; multiplies the 2 inputs using a loop and ADD
+; writes the result in output
 ;------------
 ; stack segment :
 STSEG SEGMENT
@@ -8,6 +9,9 @@ STSEG ENDS
 ; data segment :
 DTSEG SEGMENT
   ; place program data here
+  input DW 08H, 08H
+  ORG 08H
+  output DW 00H
 DTSEG ENDS
 ;------------
 ; code segment :
@@ -18,6 +22,19 @@ CDSEG SEGMENT
   MOV DS, AX
   ; main code starts here
   
+  ; initialize
+  MOV BX, OFFSET input
+  MOV CX, [BX]
+  MOV AX, 00H
+  INC BL
+  INC BL
+  
+  loop1:
+    ADD AX, [BX]
+    LOOP loop1
+  
+  MOV BX, OFFSET output
+  MOV [BX], AX
   
   ; end (terminate) program
   terminate:
