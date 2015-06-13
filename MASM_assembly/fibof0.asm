522e74b5874498057f746dcf9f8477bd853cb9b8
Mohsen Dastjerdi Zade <mail@dstjrd.ir>
2015-06-13 14:12:04 +0430

    fibonacci series starting from 0

---
+++ MASM_assembly/fibof0.asm
@@ -0,0 +1,44 @@
+;------------
+; stack segment :
+STSEG SEGMENT
+  DB 64 DUP (?)
+STSEG ENDS
+;------------
+; data segment :
+DTSEG SEGMENT
+  ; place program data here
+  DW 20 DUP (0000H)
+DTSEG ENDS
+;------------
+; code segment :
+CDSEG SEGMENT
+  MAIN PROC FAR
+  ASSUME CS:CDSEG, DS:DTSEG, SS:STSEG
+  MOV AX, DTSEG
+  MOV DS, AX
+  ; main code starts here
+  
+  ; initialize
+  MOV CX, 10
+  MOV DI, 00
+  MOV AX, 00
+  MOV DX, 01
+  
+  loop1:
+    MOV [DI], AX
+    INC DI
+    INC DI
+    MOV [DI], DX
+    INC DI
+    INC DI
+    ADD AX, DX
+    ADD DX, AX
+    LOOP loop1
+  
+  ; end (terminate) program
+  terminate:
+  MOV AH, 4CH
+  INT 21H
+  MAIN ENDP
+CDSEG ENDS
+  END MAIN
