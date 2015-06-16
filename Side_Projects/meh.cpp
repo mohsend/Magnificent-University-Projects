67eeb0933f2a0a960dd0ced633bbc52cf21c0fab
Mohsen Dastjerdi Zade <mail@dstjrd.ir>
2015-06-16 19:41:47 +0430

    meh

---
+++ Side_Projects/meh.cpp
@@ -0,0 +1,22 @@
+/*
+ * meh
+ */
+
+#include <iostream>
+#include <cstdint>
+using namespace std;
+
+int main(int argc, char **argv)
+{
+	uint64_t meh = 0xFFFFFFFFFFFFFFFF;
+	int age;
+	cout << "meh = " << meh << endl;
+	cout << "How old are you? ";
+	cin >> age;
+	cout << "meh per year = " << meh / age << endl;
+	cout << "meh per month = " << meh / (age * 12) << endl;
+	cout << "meh per day = " << meh / (age * 365) << endl;
+	cout << "meh per hour = " << meh / (age * 365 * 24) << endl;
+	cout << "meh per second = " << meh / (age * 365 * 24 * 3600) << endl;
+	return 0;
+}
