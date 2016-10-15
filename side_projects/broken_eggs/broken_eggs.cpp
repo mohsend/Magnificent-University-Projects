5078c8d0f3be1471f0092ec323615cfc931521b4
Mohsen Dastjerdi Zade <mail@dstjrd.ir>
2016-10-15 18:37:05 +0330

    Improves logic of loop

--- side_projects/broken_eggs/broken_eggs.cpp
+++ side_projects/broken_eggs/broken_eggs.cpp
@@ -8,10 +8,10 @@ using namespace std;
 
 int main()
 {
-	int temp;
-	for (int i = 1; i < 10000; i++)
+	// We can add 6 to 'i' at a time, because we know 'i' isn't divisible by 2 or 6
+	for (int i = 1; i < 10000; i += 6)
 	{
-		temp = i * 7;
+		int temp = i * 7;
 		if ( (temp % 4 == 1) && (temp % 5 == 1) && (temp % 6 == 1) )
 		{
 			cout << temp << endl;
