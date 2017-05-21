845b5ab09723e28980d52a7fb53e3f644cba8ba4
Mohsen Dastjerdi Zade <mail@dstjrd.ir>
2017-05-22 00:06:03 +0430

    Add 2nd assignment

---
+++ database_lab/gozaresh_2.sql
@@ -0,0 +1,65 @@
+﻿-----
+---- گزارش کار 2
+-----
+USE university
+
+---
+-- سوال 1
+--- در سوال 7 گزارش کار 1 انجام شده
+
+---
+-- سوال 2
+---
+
+SELECT * FROM student WHERE enter_year = 1384
+
+---
+-- سوال 3
+---
+
+SELECT * FROM professor WHERE name LIKE 'a%'
+
+---
+-- سوال 4
+---
+
+SELECT COUNT(*) FROM student WHERE enter_year = 1384
+
+---
+-- سوال 5
+---
+
+SELECT SUM(ncourse) FROM lessons WHERE student_id = 1
+
+---
+-- سوال 6
+---
+
+SELECT * FROM lessons WHERE ncourse <> 3
+
+---
+-- سوال 7
+---
+
+SELECT TOP 3 * FROM student
+
+---
+-- سوال 8
+---
+-- ستون مربوط به شماره شناسنامه در گزارش قبلی پاک شد. اگر بود:
+
+SELECT * FROM student WHERE shenasname = 0
+
+---
+-- سوال 9
+---
+
+ALTER TABLE student ADD tel VARCHAR(16)
+
+---
+-- سوال 10
+---
+
+SELECT * FROM student WHERE tel <> '0511-123456'
+
+SELECT * FROM student WHERE tel LIKE '0[0-9][0-9][0-9]-[0-9][0-9][0-9][0-9][0-9][0-9]'
